195901d3b209d66ea02b9327e9ed558c703362f6
ugis70194 <ugis70194@yahoo.co.jp>
2023-09-03 04:26:54 +0000

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import { jsonLoader } from './_utils/functions';
 import { URL_PREFIX } from './_utils/variables';
-import { generalData } from './_interfaces/interfaces';
 import Thumbnail from './_thumbnail'
 import ThumbnailTable from './_thumbnailTable'
 
 const projectName = 'test';
 
 export default async function Home() {
-  const generalData: generalData = await jsonLoader(`${URL_PREFIX}/${projectName}/generalData.json`);
+  const titles: {data: string[]} = await jsonLoader(`${URL_PREFIX}/${projectName}/general/titles.json`);
 
   return (
     <ThumbnailTable>
       {
-        (generalData.titles).map((title) => <Thumbnail projectName={projectName} title={title} key={title}/>)
+        (titles.data).map((title) => <Thumbnail projectName={projectName} title={title} key={title}/>)
       }
     </ThumbnailTable>
   );
