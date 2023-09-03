4a9960369506cd1ceacad422bc90a703b5de3698
ugis70194 <ugis70194@yahoo.co.jp>
2023-09-03 04:27:15 +0000

    状態を親から子へ流すように変更

--- src/app/_header/menu/_menu.tsx
+++ src/app/_header/menu/_menu.tsx
@@ -1,19 +1,15 @@
-import React, {createContext, useReducer } from 'react'
+import React, { useReducer } from 'react'
 import MenuButton from './_menuButton'
-import MenuInner from './_menuInner'
-
-export const MenuContext = createContext<[boolean, React.DispatchWithoutAction|undefined]>([false, undefined]);
 
 function reducer(state: boolean){
   return !state;
 }
 
 export default function Menu(){
-  const [MenuOpen, toggle] = useReducer(reducer, false);
+  const [isOpen, toggle] = useReducer(reducer, false);
   return (
-    <MenuContext.Provider value={[MenuOpen, toggle]}>
-      <MenuButton />
-      <MenuInner />
-    </MenuContext.Provider>
+    <>
+      <MenuButton isOpen={isOpen} toggle={toggle} />
+    </>
   )
 }
