0ef931ccdc7c967234b44ad31de319834daec2da
ugis70194 <ugis70194@yahoo.co.jp>
2023-09-03 04:27:30 +0000

    fix

--- src/app/_header/menu/_menuButton.tsx
+++ src/app/_header/menu/_menuButton.tsx
@@ -1,16 +1,19 @@
 import React, { useContext } from 'react';
-import { MenuContext } from './_menu'
 import { Box, Flex } from '@kuma-ui/core'
 import Image from 'next/image'
 import { HeaderHeight } from '../_header';
 
-export default function MenuButton() {
-  const [MenuOpen, toggle] = useContext(MenuContext);
+interface Props {
+  isOpen: boolean
+  toggle: React.DispatchWithoutAction
+}
+
+export default function MenuButton({isOpen, toggle}: Props) {
 
   return (
     <Box position={'relative'} onClick={toggle} height={HeaderHeight} width={HeaderHeight} margin={[0,0,0,'auto']}>
       {
-        MenuOpen
+        isOpen
         ? <Image src='/icons/cross.svg' alt='menu open' fill={true} />
         : <Image src='/icons/menu-burger.svg' alt='menu close' fill={true} />
       }
