f9fdea4307a2ba00f780eb5bcf299fd940750a92
ugis70194 <ugis70194@yahoo.co.jp>
2023-09-03 03:27:41 +0000

    ボタンの位置を右はしに来るように変更

--- src/app/_header/menu/_menuButton.tsx
+++ src/app/_header/menu/_menuButton.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { useContext } from 'react';
 import { MenuContext } from './_menu'
 import { Box, Flex } from '@kuma-ui/core'
@@ -9,7 +8,7 @@ export default function MenuButton() {
   const [MenuOpen, toggle] = useContext(MenuContext);
 
   return (
-    <Box position={'relative'} onClick={toggle} height={HeaderHeight} width={HeaderHeight} right={0}>
+    <Box position={'relative'} onClick={toggle} height={HeaderHeight} width={HeaderHeight} margin={[0,0,0,'auto']}>
       {
         MenuOpen
         ? <Image src='/icons/cross.svg' alt='menu open' fill={true} />
