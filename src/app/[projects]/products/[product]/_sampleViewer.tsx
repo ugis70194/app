fab3a3f3e427d675f4900888f380d0f568cd0fb1
ugis70194 <ugis70194@yahoo.co.jp>
2023-09-07 11:17:42 +0000

    mock

---
+++ src/app/[projects]/products/[product]/_sampleViewer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface props {
+  project: string,
+  product: string,
+  count: number,
+}
+
+export default function SampleViewer({project, product, count}: props){
+  return (
+    <>
+    </>
+  )
+}
