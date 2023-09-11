57c85128fc881d16de4c85f3d252a133fb3f0576
ugis70194 <ugis70194@yahoo.co.jp>
2023-09-11 17:38:01 +0000

    add sampleViewer

--- src/app/[projects]/products/[product]/_sampleViewer.tsx
+++ src/app/[projects]/products/[product]/_sampleViewer.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
+'use client'
 
-interface props {
-  project: string,
-  product: string,
-  count: number,
+import React, { useReducer } from 'react'
+import Image from 'next/image'
+import { Box, Text, VStack} from '@kuma-ui/core'
+
+function reducer(pageNumber: number, args: {maxPage: number, control: string | undefined}){
+  if(args.control === "next"){
+    pageNumber = (pageNumber+1) % args.maxPage;
+  }
+  if(args.control === "prev"){
+    if(pageNumber === 0){
+      pageNumber = args.maxPage - 1;
+    }else{
+      pageNumber = pageNumber - 1;
+    }
+  }
+  return pageNumber; 
 }
 
-export default function SampleViewer({project, product, count}: props){
+export default function SampleViewer({ imgURLs }:{imgURLs: string[]}){
+  const [pageNumber, controler] = useReducer(reducer, 0);
+  const maxPage = imgURLs.length;
   return (
-    <>
-    </>
+    <VStack justifyContent={'center'} margin={[0,0,0,0]}>
+      <Box position={'relative'} width={'100vw'} maxWidth={'600px'} height={'80vh'} margin={0}
+      >
+        <Image src={imgURLs[pageNumber]} alt={`page ${pageNumber+1}`} style={{objectFit: 'contain'}} fill={true} />
+        <Box position={'absolute'} top={0} left={0} height={'100%'} width={'50%'} 
+            onClick={() => controler({maxPage:maxPage, control:'prev'})}
+            bg={'green'} opacity={'0.2'} zIndex={2}
+        ></Box>
+        <Box position={'absolute'} top={0} left={'50%'} height={'100%'} width={'50%'} 
+            onClick={() => controler({maxPage:maxPage, control:'next'})}
+            bg={'red'} opacity={'0.2'} zIndex={2}
+        ></Box>
+      </Box>
+      <Text textAlign={'center'}>{`${pageNumber+1}/${maxPage}`}</Text>
+    </VStack>
   )
 }
