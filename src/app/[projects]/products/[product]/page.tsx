df5f8382e6eaa491a00d81a20f43fabf6adee557
ugis70194 <ugis70194@yahoo.co.jp>
2023-09-13 15:07:44 +0000

    suspenseで全部読み込んでから表示するように

--- src/app/[projects]/products/[product]/page.tsx
+++ src/app/[projects]/products/[product]/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { use, Suspense } from 'react'
 import { jsonLoader, typeBackgroundColor } from '../../_utils/functions'
 import { URL_PREFIX } from '../../_utils/variables'
-import { Box, HStack, Text } from '@kuma-ui/core'
+import { Box, HStack, Text, styled, css } from '@kuma-ui/core'
 import SampleViewer from './_sampleViewer'
 
 interface Detail {
@@ -32,26 +32,52 @@ function sampleURLs(productPath: string, sampleCount: number): string[]{
   return res;
 }
 
-export default async function Home({params}: {params : {projects: string, product: string}}){
-  const project = params.projects;
-  const title = params.product;
-  const detail: Detail = await jsonLoader(`${URL_PREFIX}/api/${project}/${title}/detail.json`);
-  let imgURLs = [`${URL_PREFIX}/api/${project}/${title}/cover.jpg`]
-                .concat(sampleURLs(`${URL_PREFIX}/api/${project}/${title}`, detail.sampleCount));
+async function fetchDetail(project: string, title: string): Promise<Detail> {
+  return await jsonLoader(`${URL_PREFIX}/api/${project}/${title}/detail.json`);
+}
+
+function fetchImgURLs(project: string, title: string, sampleCount: number): string[] {
+  return [`/api/${project}/${title}/cover.jpg`]
+          .concat(sampleURLs(`/api/${project}/${title}`, sampleCount));
+}
+
+const Divider = styled("div")`
+border: solid dimgray;
+border-width: 5px 0 0;
+height: 0;
+width: 100%;
+display: inline-block;
+padding: 3px;
+`;
+
+const Td = styled("td")`
+border: 1px solid gray;
+padding: 8px;
+`
+const Tr = styled("tr")`
+background: gainsboro;
+:nth-child(even){
+  background: whitesmoke;
+}
+`
+
+function Main({project, title}: {project: string, title: string}){
+  const detail: Detail = use(fetchDetail(project, title));
+  const imgURLs = fetchImgURLs(project, title, detail.sampleCount);
 
   return (
-    <Box>
-      <HStack>
+    <Box maxWidth={'1200px'} margin={'0 auto'}>
+      <HStack gap={[4,8]}>
         {
           detail.type ? 
-          <Box bg={typeBackgroundColor(detail.type)}>
+          <Box bg={typeBackgroundColor(detail.type)} p={8} color={'white'}>
               {detail.type}
             </Box> 
           : <></>
         }
         {
           detail.r18 ? 
-          <Box bg={'pink'}>R-18</Box>
+          <Box bg={'pink'} p={8} color={'white'}>R-18</Box>
           : <></>
         }
       </HStack>
@@ -64,50 +90,67 @@ export default async function Home({params}: {params : {projects: string, produc
           </HStack>
         : <></>
       }
-      <Text fontWeight={'bold'}>{detail.title}</Text>
+      <Divider />
+      <Text fontSize={['1.5em', '2em']} fontWeight={'bold'}>{detail.title}</Text>
       <SampleViewer imgURLs={imgURLs} />
+      <Divider />
       <Box>
-        <Text>作品説明</Text>
+        <Text fontSize={['1.2rem', '1.5rem']} fontWeight={'bold'}>作品説明/コメント</Text>
         <Text>
           {detail.description}
         </Text>
       </Box>
-      <table>
+      <Divider />
+      <Box alignContent={'center'}>
+      <table className={css `display:table; margin:auto;`}>
         <tbody>
-          <tr>
-            <td>作者</td>
-            <td>{concatString(detail.creator)}</td>
-          </tr>
-          <tr>
-            <td>公開日</td>
-            <td>{detail.published_at}</td>
-          </tr>
+          <Tr>
+            <Td>作者</Td>
+            <Td>{concatString(detail.creator)}</Td>
+          </Tr>
+          <Tr>
+            <Td>公開日</Td>
+            <Td>{detail.published_at}</Td>
+          </Tr>
           {
             detail.size ? 
-            <tr>
-              <td>版型・メディア</td>
-              <td>{detail.size}</td>
-            </tr>
+            <Tr>
+              <Td>版型・メディア</Td>
+              <Td>{detail.size}</Td>
+            </Tr>
             : <></>
           }
           {
             detail.page ?
-            <tr>
-              <td>ページ数・CG数・曲数</td>
-              <td>{detail.page}</td>
-            </tr>
+            <Tr>
+              <Td>ページ数・CG数・曲数</Td>
+              <Td>{detail.page}</Td>
+            </Tr>
             : <></>
           }
           {
             detail.event ? 
-            <tr>
-              <td>イベント</td>
-              <td>{detail.event}</td>
-            </tr>
+            <Tr>
+              <Td>イベント</Td>
+              <Td>{detail.event}</Td>
+            </Tr>
           : <></>
           }
         </tbody>
       </table>
+      </Box>
     </Box>
+  )
+}
+
+
+export default function Home({params}: {params : {projects: string, product: string}}){
+  const project = params.projects;
+  const title = params.product;
+
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Main project={project} title={title} />
+    </Suspense>
   )
 }
