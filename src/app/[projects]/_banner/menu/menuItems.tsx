2e1d7336af670c721c6621d7a2d7db55509021b5
ugis70194 <ugis70194@yahoo.co.jp>
2023-10-04 09:57:58 +0000

    position control for css

--- src/app/[projects]/_banner/menu/menuItems.tsx
+++ src/app/[projects]/_banner/menu/menuItems.tsx
@@ -1,8 +1,8 @@
 import React, { use } from 'react'
 import { Box, css, k } from '@kuma-ui/core'
 import Link from 'next/link'
-import { jsonLoader } from '../../_utils/functions'
-import { URL_PREFIX } from '../../_utils/variables'
+import { jsonLoader } from '../../../_utils/functions'
+import { URL_PREFIX } from '../../../_utils/variables'
 
 async function fetchProductTypes(project: string): Promise<string[]> {
   return await jsonLoader(`${URL_PREFIX}/api/${project}/general/productTypes.json`);
@@ -14,22 +14,44 @@ export default function MenuItems({
   project: string,
 }){
   //const productTypes = use(fetchProductTypes(project))
-  const productTypes = ["イラスト", "マンガ", "小説"]
+  const productTypes = ['ALL', 'イラスト', 'マンガ', '小説']
   return (
-    <k.ul width="100vw">
-    {
-      (productTypes).map((type) => (
-        <li key={type} className={css `list-style: none`} >
-          <Link href={`/${project}/products/${type}`} >
-            <Box 
-              height={'10vh'} width={'100%'} textAlign={'center'} 
-              fontSize={['2rem', '2rem', '3rem', '4rem']} >
-              {type}
-            </Box>
-          </Link>
-        </li>
-      ))
+  <Box className={css `
+    .menu-checkbox:checked ~ .menu-items {
+      transform: translateY(0);
     }
-    </k.ul>
+  `}>
+    <input type="checkbox" id="menu-toggle" className='menu-checkbox' />
+    <label htmlFor="menu-toggle" className='menu-button'>menu</label>
+    <Box
+      position={'relative'}
+      transform={'translateY(-200%)'}
+      transition={'.5s'}
+      height={'90vh'}
+      className='menu-items'  
+    >
+      <k.ul 
+        zIndex={20} m={'auto'} 
+        paddingLeft={'1rem'} paddingRight={'1rem'}>
+      {
+        (productTypes).map((type) => (
+          <li key={type} className={css `list-style: none`} >
+            <Link href={`/${project}/products/${type}`}>
+              <Box 
+                width={'100%'} 
+                textAlign={'center'} 
+                paddingTop={['0.75rem', '0.75rem', '1rem', '1rem']}
+                paddingBottom={['0.75rem', '0.75rem', '1rem', '1rem']}
+                fontSize={['1.5rem', '1.5rem', '2rem', '2rem']}
+                >
+                {type}
+              </Box>
+            </Link>
+          </li>
+        ))
+      }
+      </k.ul>
+    </Box>
+  </Box>
   )
 }
