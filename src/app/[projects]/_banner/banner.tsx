4ffb59c2692e0e67628ea457aafa6f5f790004c3
ugis70194 <ugis70194@yahoo.co.jp>
2023-10-11 08:20:39 +0000

    要素を縦に中央ぞろえするように

--- src/app/[projects]/_banner/banner.tsx
+++ src/app/[projects]/_banner/banner.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { k } from '@kuma-ui/core'
+import { css, k } from '@kuma-ui/core'
 
 export default function Banner({children}:{children: React.ReactNode}){
   return (
@@ -7,13 +7,28 @@ export default function Banner({children}:{children: React.ReactNode}){
     position={'sticky'}
     display={'flex'}
     justifyContent={'flex-end'}
+    alignItems={'center'}
     top={0}
     zIndex={20}
-    width={'100vw'}
+    width={'calc(100vw - 32px)'}
+    height={'8vh'}
     gap={'2rem'}
+    borderBottomStyle={'solid'}
+    className={mediaStyle}
     >
       {children}
     </k.nav>
     
   )
 }
+
+const mediaStyle = css `
+  @media (prefers-color-scheme: dark) {
+    background-color: t("colors.bg_dark");
+    border-color: t("colors.icon_dark");
+  },
+  @media (prefers-color-scheme: light) {
+    background-color: t("colors.bg_light");
+    border-color: t("colors.icon_light");
+  }
+`
