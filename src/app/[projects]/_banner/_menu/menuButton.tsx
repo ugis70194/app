4dedec277ec069e9325906821774bb5f4b3192b5
ugis70194 <ugis70194@yahoo.co.jp>
2023-10-11 08:21:22 +0000

    大きさを変更

--- src/app/[projects]/_banner/_menu/menuButton.tsx
+++ src/app/[projects]/_banner/_menu/menuButton.tsx
@@ -32,8 +32,8 @@ const buttonStyle = css `
 .menu-button {
   display: block;
   position: relative;
-  width: 26px;
-  height: 26px;
+  width: 2rem;
+  height: 3rem;
   cursor: pointer;
   & > span, & > span::before, & > span::after{
     transition: .3s;
@@ -48,13 +48,17 @@ const buttonStyle = css `
       background-color: t("colors.icon_light");
     }
   },
+  & > span {
+    content: '';
+    top: 1.5rem;
+  }
   & > span::before {
     content: '';
-    top: -8px;
+    top: -0.5rem;
   },
   & > span::after {
     content: '';
-    top: 8px;
+    top: 0.5rem;
   },
 },
 `
